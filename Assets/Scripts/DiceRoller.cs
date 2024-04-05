28161d57ff8b956beb61eea5ed20800002810590
hunterdyar <bloop@hdyar.com>
2024-04-04 22:50:41 -0400

    remove visual scripting

--- Assets/Scripts/DiceRoller.cs
+++ Assets/Scripts/DiceRoller.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Blooper.Utilities;
 using HDyar.DiceRoller.RollCodeParser;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
